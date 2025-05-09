8d9fb3f4c50c06a8ad29bea45efec274d532657b
alinjar1996 <alinjardannitdgp2014@gmail.com>
2025-05-09 17:17:35 +0300

    Model added and initial code for Task written. Not working Yet. Issue #1

--- hydrax/tasks/ur5.py
+++ hydrax/tasks/ur5.py
@@ -32,34 +32,14 @@ class UR5(Task):
         """Get the End Effector Frame (EEF) position."""
         tcp_pos = state.site_xpos[self.tcp_id]
 
+        return tcp_pos
+
     def running_cost(self, state: mjx.Data, control: jax.Array) -> jax.Array:
         """The running cost ℓ(xₜ, uₜ)."""
         orientation_cost = jnp.sum(jnp.square(self._get_eef_rot(state)))
 
-        return 100.0 * orientation_cost
+        return 100.0 * orientation_cost + 100.0 * jnp.sum(jnp.square(self._get_eef_pos(state)))
 
     def terminal_cost(self, state: mjx.Data) -> jax.Array:
         """The terminal cost ϕ(x_T)."""
         return self.running_cost(state, jnp.zeros(self.model.nu))
-
-    # def domain_randomize_model(self, rng: jax.Array) -> Dict[str, jax.Array]:
-    #     """Randomize the friction parameters."""
-    #     n_geoms = self.model.geom_friction.shape[0]
-    #     multiplier = jax.random.uniform(rng, (n_geoms,), minval=0.5, maxval=2.0)
-    #     new_frictions = self.model.geom_friction.at[:, 0].set(
-    #         self.model.geom_friction[:, 0] * multiplier
-    #     )
-    #     return {"geom_friction": new_frictions}
-
-    # def domain_randomize_data(
-    #     self, data: mjx.Data, rng: jax.Array
-    # ) -> Dict[str, jax.Array]:
-    #     """Randomly perturb the measured base position and velocities."""
-    #     rng, q_rng, v_rng = jax.random.split(rng, 3)
-    #     q_err = 0.001 * jax.random.normal(q_rng, (7,))
-    #     v_err = 0.001 * jax.random.normal(v_rng, (6,))
-
-    #     qpos = data.qpos.at[0:7].set(data.qpos[0:7] + q_err)
-    #     qvel = data.qvel.at[0:6].set(data.qvel[0:6] + v_err)
-
-    #     return {"qpos": qpos, "qvel": qvel}
